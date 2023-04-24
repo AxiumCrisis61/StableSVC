af2399a3f37bb28a5dec366b3df652764befc004
AxiumCrisis61 <Jackchen2000611@gmail.com>
2023-04-24 20:56:10 +0800

    modify extract_whisper.py and .gitignore

--- code/preprocess/extract_whisper.py
+++ code/preprocess/extract_whisper.py
@@ -93,7 +93,7 @@ def extract_whisper_features(dataset, dataset_type, arguments):
 
         # save each sample's Whisper embedding respectively
         for index in range(batch_size):
-            torch.save(whisper_features[start+index], os.path.join(output_dir, "{}.pth".format(index)))
+            torch.save(whisper_features[index], os.path.join(output_dir, "{}.pth".format(start+index)))
 
     # Mapping to MCEP's lengths [WARN: Not maintained.]
     if WHISPER_MAPPED:
